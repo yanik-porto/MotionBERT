5892db82da82ba69e6a89feea8e861d10c2e9147
Yannick PORTO <yporto@teb-online.com>
2023-06-01 13:39:12 +0200

    load state dict for action model

--- infer_wild_action.py
+++ infer_wild_action.py
@@ -14,8 +14,8 @@ from lib.utils.vismo import render_and_save
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default="configs/pose3d/MB_ft_h36m_global_lite.yaml", help="Path to the config file.")
-    parser.add_argument('-e', '--evaluate', default='checkpoint/pose3d/FT_MB_lite_MB_ft_h36m_global_lite/best_epoch.bin', type=str, metavar='FILENAME', help='checkpoint to evaluate (file name)')
+    parser.add_argument("--config", type=str, default="configs/action/MB_ft_NTU60_xsub.yaml", help="Path to the config file.")
+    parser.add_argument('-e', '--evaluate', default='checkpoint/action/MB_ft_NTU60_xsub/best_epoch.bin', type=str, metavar='FILENAME', help='checkpoint to evaluate (file name)')
     parser.add_argument('-j', '--json_path', type=str, help='alphapose detection result json path')
     parser.add_argument('-v', '--vid_path', type=str, help='video path')
     parser.add_argument('-o', '--out_path', type=str, help='output path')
@@ -29,12 +29,13 @@ opts = parse_args()
 args = get_config(opts.config)
 
 model_backbone = load_backbone(args)
+
+model = ActionNet(backbone=model_backbone, dim_rep=args.dim_rep, num_classes=args.action_classes, dropout_ratio=args.dropout_ratio, version=args.model_version, hidden_dim=args.hidden_dim, num_joints=args.num_joints)
 if torch.cuda.is_available():
-    model_backbone = nn.DataParallel(model_backbone)
-    model_backbone = model_backbone.cuda()
+    model = nn.DataParallel(model)
+    model = model.cuda()
 
-print('Loading checkpoint', opts.evaluate)
-checkpoint = torch.load(opts.evaluate, map_location=lambda storage, loc: storage)
-
-print(checkpoint)
-
+print('Loading checkpoint', chk_filename)
+checkpoint = torch.load(chk_filename, map_location=lambda storage, loc: storage)
+model.load_state_dict(checkpoint['model'], strict=True)
+model.eval()
